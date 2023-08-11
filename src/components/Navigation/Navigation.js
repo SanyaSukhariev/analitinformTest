342100dadc6731d12cc4615cfc7293f56d427f2e
Oleksandr Sukhariev <sukhariev0212@gmail.com>
2023-08-11 14:44:46 +0300

    t
    t

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,24 +1,45 @@
 import { Link } from "react-router-dom";
 import classes from './Navigation.module.css'
 import {AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import logo from '../../image/icons/log.jpg'
+
 
 
 export function Navigation(){
 
     const [active, setActive]=useState(false)
+    
+    const [navbarBackground, setNavbarBackground] = useState('rgba(0, 128, 128, 1)');
+
+  //   const handleScroll = () => {
+  //     const scrolled = window.scrollY;
+  
+  //     if (scrolled > 100) {
+  //       setNavbarBackground('rgba(0, 128, 128, 1)');
+  //     } else {
+  //       setNavbarBackground('rgba(0, 128, 128, 0.2)');
+  //     }
+  //   };
+  
+  //  useEffect(() => {
+  //     window.addEventListener('scroll', handleScroll);
+  //     return () => {
+  //       window.removeEventListener('scroll', handleScroll);
+  //     };
+  //   }, []);
+    // rgba(0, 128, 128, 0.2)
 
     return(
         <header className={classes.header}>
         
             
       
-            <div className={classes.box}>
+            <div className={classes.box} style={{backgroundColor:navbarBackground}}>
                     <div >
                         <Link className={classes.logo} to='/'>
                                 <img className={classes.img} src={logo} alt="logo" /> 
-                            Analitinform 
+                            <span>Analitinform </span>
                         </Link>
                     </div>
                     <ul onClick={()=>setActive(!active)} className={active? [classes.menu, classes.active].join(' '): [classes.menu]}>
